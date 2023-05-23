7c6dd9e1d556a2ecfff3df469a3061f6375f8bb6
isurumanod <isurumanod99@gmail.com>
2023-05-23 06:46:33 +0530

    header css update

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt=""
+                className="header__logo"/>
+            <div className="header__search">
+                <input type="text" className="header__searchInput"/>
+                <div className="header__searchIcon__container">
+
+                    <img
+                        src="https://www.freeiconspng.com/thumbs/search-icon-png/search-icon-png-21.png"
+                        className="header__searchIcon"/>
+                </div>
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+                 <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBpV_k3-Ct7iHbxNS-SY_znA9SmW6PMg-w",
+//   authDomain: "clone-202305.firebaseapp.com",
+//   projectId: "clone-202305",
+//   storageBucket: "clone-202305.appspot.com",
+//   messagingSenderId: "620110681681",
+//   appId: "1:620110681681:web:ae9ccf40d884d27e2e8a70",
+//   measurementId: "G-866KGS6SKR"
+// };
+
+// Initialize Firebase
