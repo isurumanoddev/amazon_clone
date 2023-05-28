c6816b27a0c456aa1f9c96173a656a40ae6907f4
isurumanod <isurumanod99@gmail.com>
2023-05-28 10:04:08 +0530

    login form complete

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Login.css"
 import {Link} from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const signIn = (e) => {
+        e.preventDefault()
+    }
+    const register = (e) => {
+
+        e.preventDefault()
+
+    }
     return (
         <div className="login">
             <Link to="/">
@@ -16,24 +27,29 @@ function Login() {
                 <h1>Sign-in</h1>
                 <form action="" className="login__form">
                     <label className="login__form__label">E-mail</label>
-                    <input type="text" className="login__form__input"/>
+                    <input value={email} onChange={e => setEmail(e.target.value)} type="text"
+                           className="login__form__input"/>
 
                     <label className="login__form__label">Password</label>
-                    <input type="password" className="login__form__input"/>
+                    <input value={password} onChange={e => setPassword(e.target.value)} type="password"
+                           className="login__form__input"/>
 
-                    <button>Sign In</button>
+                    <button onClick={signIn} className="login__button">Sign In</button>
                 </form>
                 <label htmlFor="agree-checkbox" className="login__form__label" id={"checkbox"}>
                     <input type="checkbox" id="agree-checkbox" className="login__form__checkbox" required/>
-                    I agree to the terms and conditions By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
+                    I agree to the terms and conditions By continuing, you agree to Amazon's Conditions of Use and
+                    Privacy Notice.
                 </label>
+                <div className="login__new__container">
+                    <hr className={"horizontal-line"}/>
+                    <p className="name">New to Amazon?</p>
+                    <hr className={"horizontal-line"}/>
 
-                <button className="login__registerBtn">Create your Amazon Account</button>
-                <div className="container">
-                    <hr className="horizontal-line">
-                        <h1 className="name">John Doe</h1>
-                        <hr className="horizontal-line">
                 </div>
+
+                <button onClick={register} className="login__registerBtn">Create your Amazon Account</button>
+
             </div>
         </div>
     );
