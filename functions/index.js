8e03e8e25a38617fe9118618950c9d6fee43db16
isurumanod <isurumanod99@gmail.com>
2023-05-30 13:30:17 +0530

    Create post request for make a payment via stripe

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
-
-
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const {response} = require("express");
+const {PaymentIntent} = require("@stripe/stripe-js");
 const stripe = require("stripe")(
     "sk_test_51NDDeGCTBPVFaxN9WvYI1xEVlgBKM8NiwEFketIvihLIFwjZ" +
     "YuTPqJZ1kVq99waJ6bnPAcz2FWFodkoVUZvOoEfG00slt24exQ");
@@ -20,8 +20,20 @@ app.use(express.json());
 
 // - Api routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
+app.post(
+    "/payments/create",
+    async (request, response) => {
+      const total = request.query.total;
+      console.log("payments request received ", total);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd"});
+      response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
 
-// - Listen command
+// Listen command
 
 exports.api = functions.https.onRequest(app);
