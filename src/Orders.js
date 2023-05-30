6ca49878f8c84b2e28d7b42f858d2ddf605b7aaa
isurumanod <isurumanod99@gmail.com>
2023-05-30 19:16:32 +0530

    After Make payment render orders page/dis[lay order

--- src/Orders.js
+++ src/Orders.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "/styles/Orders.css"
+import {useStateValue} from "./StateProvider";
+import CheckoutProduct from "./Components/CheckoutProduct";
 
 function Orders() {
+    const [{basket}, dispatch] = useStateValue()
     return (
-        <div>
+        <div className="orders">
             <h1>Orders page</h1>
+            {basket.map((item) =>
+
+                <CheckoutProduct id={item.id} image={item.image} title={item.title} price={item.price}/>
+            )}
+
+
         </div>
     );
 }
