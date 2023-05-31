44d6fbf871d2320818367664434d27774b36d296
isurumanod <isurumanod99@gmail.com>
2023-05-31 11:19:32 +0530

    Retrieve data from firebase firestore with "getDocs"

--- src/Orders.js
+++ src/Orders.js
@@ -1,31 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import "./styles/Orders.css"
 import {useStateValue} from "./StateProvider";
-import CheckoutProduct from "./Components/CheckoutProduct";
-import {doc, getDoc} from "firebase/firestore";
+
+import {collection, doc, getDocs} from "firebase/firestore";
 import {db} from "./firebase";
 
 
 function Orders() {
-    const [{basket, user}, dispatch] = useStateValue()
+    const [{basket, user}, dispatch] = useStateValue();
 
     const [orders, setOrders] = useState([])
 
-    useEffect(async () => {
-        const docRef = doc(db, "users", user?.uid);
-        const docSnap =await getDoc(docRef)
+    useEffect(() => {
+        if (user) {
+            const usersCollectionRef = collection(db, "users");
+            const userDocRef = doc(usersCollectionRef, user?.uid);
+            const orderCollectionRef = collection(userDocRef, "order");
+            console.log("orderCollectionRef : ", orderCollectionRef)
+            getDocs(orderCollectionRef)
+                .then(snapShot => {
+                    setOrders(snapShot.docs.map(doc => ({
+                        id:doc.id,
+                        data:doc.data()
+                    })))
+                    console.log("snapShot : ",snapShot)
+                })
 
-        console.log("Document data:", docSnap);
+        } else {
+            setOrders([])
+        }
 
-    }, [])
+
+    }, [user])
 
     return (
         <div className="orders">
             <h1>Orders page</h1>
-            {orders.map((item) =>
-
-                <CheckoutProduct id={item.id} image={item.image} title={item.title} price={item.price}/>
-            )}
 
 
         </div>
